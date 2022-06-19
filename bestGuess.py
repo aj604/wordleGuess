0db3edb87b3e5b3e688959433dbb1badc14b8072
Avery Jones <averyjonesphoto+1@mac.com>
2022-06-19 02:24:44 +0000

    Basic 2d letter scores. Now frequency is 2d array based on position in word

--- bestGuess.py
+++ bestGuess.py
@@ -1,11 +1,38 @@
 from collections import Counter
 
+# Scoring Function
+# Takes in a list of dictionarys with corresponding scores by letter position
 def wordScore (word, freq) -> int:
     score = 0
-    for letter in set(word):
-        score += freq[letter]
+    for letter in word:
+      for i in range(0,4):
+        score += freq[i][letter]
     return score
 
+# Letter Scoring Function
+# Returns a list of dictionarys with letter scores by poition
+def letterScore(wordList):
+  scores = []
+  for i in range(1,5):
+    scores.append(nthLetterOfWords(i, wordList))
+  return scores
+  
+def possibleWords(wordList):
+  return wordList
+
+# 1 based
+def nthLetterOfWords(n, wordList):
+  letters = ""
+  for word in wordList:
+    letters += word[n-1]
+  return Counter(letters)
+  
+
+
+
+# Main Function
+# Takes in letters within the word, and letters not in the word
+# Outputs possible words remaining, and scores them based on letter frequency
 def bestGuess(goodLetters, badLetters):
     wordList = {"added", 
             "agent", 
@@ -65,8 +92,8 @@ def bestGuess(goodLetters, badLetters):
             "value", 
             "vests", 
             "yield"}
-    out = "".join(wordList)
-    store = Counter(out)
+
+    store = letterScore(wordList)
 
     badLettersArray = list(badLetters)
     goodLettersArray = list(goodLetters)
@@ -91,3 +118,63 @@ def bestGuess(goodLetters, badLetters):
     for key in res:
         print(f'{key} - Score: {res[key]}')
 bestGuess("","")
+wordList = {"added", 
+            "agent", 
+            "alpha", 
+            "asset", 
+            "audit", 
+            "basis", 
+            "board", 
+            "bonus", 
+            "brand", 
+            "check", 
+            "close", 
+            "count", 
+            "cover", 
+            "curve", 
+            "cycle", 
+            "debit", 
+            "delta", 
+            "draft", 
+            "entry", 
+            "equal", 
+            "error", 
+            "files", 
+            "first", 
+            "flows", 
+            "funds", 
+            "gross", 
+            "hedge", 
+            "index", 
+            "issue", 
+            "labor", 
+            "limit", 
+            "loans", 
+            "model", 
+            "money", 
+            "offer", 
+            "order", 
+            "point", 
+            "price", 
+            "rates", 
+            "ratio", 
+            "risks", 
+            "right", 
+            "round", 
+            "sales", 
+            "scale", 
+            "scope", 
+            "share", 
+            "sheet", 
+            "shock", 
+            "stock", 
+            "swaps", 
+            "taxes", 
+            "terms", 
+            "trade", 
+            "trust", 
+            "value", 
+            "vests", 
+            "yield"}
+
+#print(letterScore(wordList))
